98026c7239314a31d99e5fa94fc7fe61cf575550
Jcirona <jcirona@hotmail.com>
2022-02-02 11:30:21 +1100

    updated planning - initial coding

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-function makeBoxRed(event) {
-    var boxClicked = event.target
-    if (boxClicked.tagName === 'DIV')
-    boxClicked.style.backgroundColor = 'red'
-}
+var playerOneTurn = true;
+var playerTwoTurn = false;
 
-var selectedSquare = document.querySelector('.squares')
-sqaures.addEventListener('click', makeBoxRed)
+if (playerOneTurn === true){
+    var playerOneSelect = document.querySelector('.tile')
+    function makeBoxRed(event) {
+        var boxClicked = event.target
+        if (boxClicked.tagName === 'DIV')
+        boxClicked.textContent = 'X'
+    }
+
+    var selectedSquare = document.querySelector('.squares')
+    selectedSquare.addEventListener('click', makeBoxRed)
+
+}
