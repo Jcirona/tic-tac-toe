442f82a067e4b70ca1552ea7852e5eb25e79c98d
Jcirona <jcirona@hotmail.com>
2022-02-02 14:01:09 +1100

    functioning click event-no overwriting prior squares

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-var playerOneTurn = true;
-var playerTwoTurn = false;
+var playerOneTurn = true
+var winConditions = []
 
-if (playerOneTurn === true){
-    var playerOneSelect = document.querySelector('.tile')
-    function makeBoxRed(event) {
-        var boxClicked = event.target
-        if (boxClicked.tagName === 'DIV')
-        boxClicked.textContent = 'X'
+
+var playerOneSelect = document.querySelector('.tile')
+var selectedSquare = document.querySelector('.squares')
+
+
+// function to place an X or a O -- changes each click and doesn't overwrite previously selected squares
+
+
+
+function playerOneClick(event) {
+    var boxClicked = event.target
+    if (boxClicked.tagName === 'DIV') {
+        if (boxClicked.textContent != 'O' && playerOneTurn === true) {
+            boxClicked.textContent = 'X'
+            playerOneTurn = false 
+        }else if(boxClicked.textContent != 'X' && playerOneTurn === false) {
+            boxClicked.textContent = 'O'    
+            playerOneTurn = true
+        }
     }
-
-    var selectedSquare = document.querySelector('.squares')
-    selectedSquare.addEventListener('click', makeBoxRed)
-
 }
+
+selectedSquare.addEventListener('click', playerOneClick)
+
