c046aa34d14f5c70137a9233d22781b9d7be97f3
Jcirona <jcirona@hotmail.com>
2022-02-03 16:06:21 +1100

    Adding custom sounds to game

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var turnNum = 0
 var playerOneScore = 0
 var playerTwoScore = 0
 
+
+
+
 var tileOne = document.querySelector('.tile-one')
 var tileTwo = document.querySelector('.tile-two')
 var tileThree = document.querySelector('.tile-three')
@@ -32,6 +35,8 @@ function playerOneClick(event) {
     var boxClicked = event.target
     if (boxClicked.tagName === 'DIV') {
         if (boxClicked.textContent != 'O' && playerOneTurn === true) {
+            var p1Audio = new Audio('/Users/jeremey/sei/project1/sounds/PlayerOne-Select.mp3')
+            p1Audio.play()
             boxClicked.textContent = 'X'
             boxClicked.style.color = 'yellow'
             boxClicked.style.animationName = 'rainbowSquareX'
@@ -78,6 +83,8 @@ function playerOneClick(event) {
 
          // Player O's seriously large nest                               
         }else if(boxClicked.textContent != 'X' && playerOneTurn === false) {
+            var p1Audio = new Audio('/Users/jeremey/sei/project1/sounds/PlayerTwo-Select.mp3')
+            p1Audio.play()
             boxClicked.textContent = 'O'
             boxClicked.style.color = 'orange'
             boxClicked.style.animationName = 'rainbowSquareX'
@@ -123,11 +130,13 @@ function playerOneClick(event) {
     }
     if (playerOneWins === true) {
         resultBoard.textContent = 'Player One Wins!'
+        resultBoard.style.color = 'yellow'
         playerOneScore ++
         playerOneNewScore.textContent = playerOneScore
         selectedSquare.removeEventListener('click', playerOneClick)
     } else if (playerTwoWins === true) {
         resultBoard.textContent = 'Player Two Wins!'
+        resultBoard.style.color = 'orange'
         playerTwoScore ++
         playerTwoNewScore.textContent = playerTwoScore
         selectedSquare.removeEventListener('click', playerOneClick)
@@ -157,10 +166,36 @@ function clearBoard () {
     turnNum = 0
     selectedSquare.addEventListener('click', playerOneClick)
     playerOneTurn = true
-    selectedSquare.classList.remove('squares-animation')
-    setTimeout( function() {
-        selectedSquare.classList.add('squares-animation')
-    }, 10)
+
+
+
+    //Just for lol's horribly primitive animation code -> shrinks boxes on reset
+    tileOne.style.animationName = 'boardReset'
+    tileOne.style.animationDuration = '.2s'
+
+    tileTwo.style.animationName = 'boardReset'
+    tileTwo.style.animationDuration = '.2s'
+
+    tileThree.style.animationName = 'boardReset'
+    tileThree.style.animationDuration = '.2s'
+
+    tileFour.style.animationName = 'boardReset'
+    tileFour.style.animationDuration = '.2s'
+
+    tileFive.style.animationName = 'boardReset'
+    tileFive.style.animationDuration = '.2s'
+
+    tileSix.style.animationName = 'boardReset'
+    tileSix.style.animationDuration = '.2s'
+
+    tileSeven.style.animationName = 'boardReset'
+    tileSeven.style.animationDuration = '.2s'
+
+    tileEight.style.animationName = 'boardReset'
+    tileEight.style.animationDuration = '.2s'
+
+    tileNine.style.animationName = 'boardReset'
+    tileNine.style.animationDuration = '.2s'
     
     
 }
